1f96822b64403b1f3578102da87dbbc780e9ca78
cho youngjune <yojucho.jp@gmail.com>
2021-11-25 13:33:16 +0900

    created

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # powershell
+Windows Server 를 운용하면서 업무의 편의를 위해 만든 스크립트
---
+++ deletelogfiles.ps1
@@ -0,0 +1,38 @@
+###############################################################################
+#설명: 지정 한 파일및 폴더의 삭제와 압축 스크립트
+#설정: 윈도우 스케쥴러를 사용해 스크립트를 자동 실행
+# Action: Start a program
+# Program/script: %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe
+# Add arguments: -Command ".\deletelogfiles.ps1"
+# Start in: 파워쉘 스크립트가 놓여 있는 폴더
+###############################################################################
+
+## 필요한 날짜값 취득
+$bzip2_date = (Get-Date).AddDays(-1).ToString("yyyyMMdd") 
+$delete_date = (Get-Date).AddDays(-14).ToString("yyyyMMdd") 
+#Write-Host ===== Delete data files And log folder on gameoserver =====
+#Write-Host 'delete: '$delete_date
+
+# 경로설정
+$path = "D:\Log"
+$dst_folder = $path + "\" + $delete_date
+$compressLog = $path + "\" + $bzip2_date
+
+# Delete files: 지정 확장자의 파일을 삭제한다
+if (Test-path $path)
+{
+    Remove-Item $path\debugValue*.dat -Force
+}
+
+# Delete log folder: 설정한 날짜의 폴더가 있으면 삭제한다
+if (Test-path $dst_folder)
+{
+    Remove-Item -Path $dst_folder -Recurse -Force    
+}
+
+# Compress logfiles: 지정 경로 밑에 txt 파일들 압축
+set-location "C:\Windows\system32" ## 해당 경로에 bzip2.exe 파일이 있어야 압축을 진행
+if (Test-path $compressLog)
+{
+    bzip2.exe -z  $compressLog\logfile*.txt
+}
