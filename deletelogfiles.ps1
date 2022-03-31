fe69a141cc7d6d7359d6af9e9e6a20646aa59c5c
Cho.Youngjune <42788315+yojucho@users.noreply.github.com>
2022-03-31 16:21:08 +0900

    Update deletelogfiles.ps1

--- deletelogfiles.ps1
+++ deletelogfiles.ps1
@@ -1,33 +1,50 @@
-###################################
-# 지정 한 파일및 폴더의 삭제와 압축
-###################################
+$system32 = "C:\Windows\system32"
 
-## 필요한 날짜값 취득
-$bzip2_date = (Get-Date).AddDays(-1).ToString("yyyyMMdd") 
-$delete_date = (Get-Date).AddDays(-14).ToString("yyyyMMdd") 
-#Write-Host ===== Delete data files And log folder on gameoserver =====
-#Write-Host 'delete: '$delete_date
 
-# 경로설정
-$path = "D:\Log"
-$dst_folder = $path + "\" + $delete_date
-$compressLog = $path + "\" + $bzip2_date
+##### Logserver #####
+$logserver = "D:\LogServer"
 
-# Delete files: 지정 확장자의 파일을 삭제한다
-if (Test-path $path)
-{
-    Remove-Item $path\debugValue*.dat -Force
+#compress logfiles
+$compressDate = (Get-Date).AddMonths(-1).ToString("yyyyMMdd")
+$logfolder = $logserver + $compressDate
+if (Test-path $logfolder)
+{    
+    cd $system32
+    bzip2.exe -z $logfolder\*.txt
 }
 
-# Delete log folder: 설정한 날짜의 폴더가 있으면 삭제한다
-if (Test-path $dst_folder)
+#delete logfiles
+$deleteDate = (Get-Date).AddMonths(-3).ToString("yyyyMMdd")
+$logfolder = ""
+$logfolder = $logserver + "\" + $deleteDate
+
+if (Test-Path $logfolder)
 {
-    Remove-Item -Path $dst_folder -Recurse -Force    
+    Remove-Item $logfolder -Recurse -Force
 }
 
-# Compress logfiles: 지정 경로 밑에 txt 파일들 압축
-set-location "C:\Windows\system32" ## 해당 경로에 bzip2.exe 파일이 있어야 압축을 진행
-if (Test-path $compressLog)
+
+##### Logserver #####
+$logserver = "D:\LOG\BACKUP"
+
+#compress logfiles
+$compressDate = ""
+$compressDate = (Get-Date).AddMonths(-6).ToString("yyyyMM")
+if (Test-path $logserver)
+{    
+    $pre_files = $logserver + "\" + $compressDate + "*.pre"
+    
+    cd $system32    
+    bzip2.exe -z $pre_files
+}
+
+#delete logfiles
+$deleteDate = ""
+$deleteDate = (Get-Date).AddMonths(-7).ToString("yyyyMM")
+if (Test-Path $logserver)
 {
-    bzip2.exe -z  $compressLog\logfile*.txt
+    $pre_files = $logserver + "\" + $deleteDate + "*.bz2"
+
+    cd $system32
+    Remove-Item $pre_files -Recurse -Force
 }
